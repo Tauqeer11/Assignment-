5f5be6becd1e48a118a52945c95137cac358f01f
Tauqeer11 <139426574+Tauqeer11@users.noreply.github.com>
2023-07-14 11:49:55 +0500

    Add files via upload

---
+++ Assignment. c.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int isprime(int num) {
+    if (num < 2) {
+       return 0;
+}
+    for (int i = 2; i * i <= num; i++) {
+    if (num % i == 0) {
+       return 0;
+}
+}
+       return 1;
+}
+
+int main() {
+    int number;
+    while (1) {
+    printf("A number between 2 and 100: ");
+    scanf("%d", &number);
+    if (number < 2 || number > 100) {
+    printf("Number is not in the range,\n press 1 to try again: ");
+    int choice;
+    scanf("%d", &choice);
+    if (choice != 1) {
+    break;
+}
+} else {
+    int result = isprime(number);
+    printf("function returns: %d",isprime(number));break;
+}
+}
+     return 0;
+}
